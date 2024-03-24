199c828f9cff6e2cb636bcd5b660ad0c4078b445
kfoq7 <frbrizoi@gmail.com>
2024-03-23 21:05:52 -0500

    feat: added icons

--- packages/next-app/components/aside.tsx
+++ packages/next-app/components/aside.tsx
@@ -1,18 +1,17 @@
 'use client'
 
 import { NotePreviewList } from './note-preview-list'
-
-// const NotePreviewList = dynamic(() => import('./note-preview-list'), { ssr: false })
+import { AddIcon, DeleteIcon } from '@/icons'
 
 export function Aside() {
   return (
     <aside className="flex h-screen w-[290px] flex-col">
       <div className="flex items-center justify-between p-3">
         <button className="rounded-md border border-zinc-400/50 px-2 py-1 transition-colors duration-100 hover:bg-zinc-600/50">
-          New
+          <AddIcon />
         </button>
         <button className="rounded-md border border-zinc-400/50 px-2 py-1 transition-colors duration-100 hover:bg-zinc-600/50">
-          Delete
+          <DeleteIcon />
         </button>
       </div>
 
